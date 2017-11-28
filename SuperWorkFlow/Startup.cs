5e1b3835a911a3a2c2e84ea4c91e901632e4e99e
yang yu <yuyang8986@hotmail.com>
2017-11-28 20:14:12 +1100

    Add project files.

---
+++ SuperWorkFlow/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperWorkFlow.Startup))]
+namespace SuperWorkFlow
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
